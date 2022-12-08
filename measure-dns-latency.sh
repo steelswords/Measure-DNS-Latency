6ba4c35e9f3b61a8e73b0eb61df2547e3b28e419
Tristan Andrus <tristan@tristanandrus.com>
2022-12-08 09:23:20 -0700

    Handle blocked or dead domains
    Signed-off-by: Tristan Andrus <tristan@tristanandrus.com>

--- measure-dns-latency.sh
+++ measure-dns-latency.sh
@@ -2,6 +2,11 @@
 
 set -ue -o pipefail
 
+# Uncomment to see each command executed
+#set -x
+
+trap 'echo An error occurred! Quitting prematurely.' ERR
+
 # Get a relative path to the domain list, no matter where the script is called from.
 domainList="$(dirname $0)/public-domain-lists/opendns-random-domains.txt"
 numberOfDomains=100
@@ -15,7 +20,10 @@ domains="$(shuf -n $numberOfDomains $domainList)"
 i=0
 for domain in $domains
 do
-  lookupTime="$( ( time nslookup $domain ) |& awk '$1=="real"{print $2}' )"
+  echo "Trying $domain"
+  # There are some dead domains in our domain list, so we have to be prepared to break
+  # out if nslookup fails
+  lookupTime="$( ( time nslookup $domain ) |& awk '$1=="real"{print $2}' )" || continue
   echo "$i $domain $lookupTime" | tee --append "$dataFile"
   i=$(( i + 1 ))
 done
